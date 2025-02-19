571e07c6767cbba590dcb9c964d43806cf3af143
Jayrek <jrektabasa@gmail.com>
2025-02-20 07:03:29 +0800

    add blocConsumerNavigationKey to constants

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,5 +4,6 @@ const String cubitNavigationKey = '/cubit';
 const String blocNavigationKey = '/bloc';
 const String blocBuilderNavigationKey = '/blocBuilder';
 const String blocListenerNavigationKey = '/blocListener';
+const String blocConsumerNavigationKey = '/blocConsumer';
 const String blocSelectorNavigationKey = '/blocSelector';
 const String repositoryProviderrNavigationKey = '/repositoryProvider';
