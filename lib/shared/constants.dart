8fba37166f6be3a7ace2df6004e06aca66e8db27
Jayrek <jrektabasa@gmail.com>
2025-02-19 18:09:50 +0800

    add navigation keys constant

---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+// navigation keys
+const String homeNavigationKey = '/';
+const String cubitNavigationKey = '/cubit';
+const String blocNavigationKey = '/bloc';
+const String blocBuilderNavigationKey = '/blocBuilder';
+const String blocListenerNavigationKey = '/blocListener';
+const String blocSelectorNavigationKey = '/blocSelector';
+const String repositoryProviderrNavigationKey = '/repositoryProvider';
